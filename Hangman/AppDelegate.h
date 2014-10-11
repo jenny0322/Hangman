4834661fdef7a557f128bd539221651e785169ba
Jenny Chen <jennychen0322@gmail.com>
2014-10-11 16:51:12 -0700

    Initial Commit

---
+++ Hangman/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hangman
+//
+//  Created by Jenny Chen on 10/11/14.
+//  Copyright (c) 2014 Jenny Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
